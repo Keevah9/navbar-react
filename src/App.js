912cfafe61527767c65439865456b6e49691d43d
Ogunniyi Veronica <86062530+Keevah9@users.noreply.github.com>
2022-08-06 09:32:30 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import "./App.css";
+import NavBar from "./NavBar";
 
-import './App.css';
-import NavBar from './NavBar';
-// import UseRef from './UseRef';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <UseRef /> */}
+        
         <NavBar />
       </header>
     </div>
